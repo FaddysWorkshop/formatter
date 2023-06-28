aeac431513fefc371bab8daf2039d7b59bd1d8d6
mariobassem <mariobassem12@gmail.com>
2023-06-28 04:39:50 +0300

    implement formatter cli

--- formatter.js
+++ formatter.js
@@ -1,251 +1,401 @@
 import "fs";
 import { readFileSync } from "fs";
 import jsTokens from "js-tokens";
-
+import Scenarist from "scenarist.dev";
 const NonSpecialToken = "NonSpecialToken";
 const LineFormatter = "LineFormatter";
-
 const left = {
-	"]": "[",
-	")": "(",
-	"}": "{",
+
+"]" : "[",
+")" : "(",
+"}" : "{",
+
 };
 
-const declarationKeyWords = ["var", "let", "const"];
+const declarationKeyWords = [ "var", "let", "const" ];
 const assignmentOperators = [
-	"=",
-	"+=",
-	"-=",
-	"*=",
-	"/=",
-	"%=",
-	"**=",
-	"<<=",
-	">>=",
-	">>>=",
-	"&=",
-	"^=",
-	"|=",
-	"&&=",
-	"||=",
-	"??=",
+
+"=",
+"+=",
+"-=",
+"*=",
+"/=",
+"%=",
+"**=",
+"<<=",
+">>=",
+">>>=",
+"&=",
+"^=",
+"|=",
+"&&=",
+"||=",
+"??=",
 ];
+const PunctuatorType = {
+
+":" : "Colon",
+"++" : "IncrementDecrement",
+"--" : "IncrementDecrement",
+";" : "SemiColon",
+"," : "Comma",
+"[" : "OpeningSquareBracket",
+"]" : "Closing",
+"}" : "Closing",
+")" : "Closing",
+
+}
 
 export default class Formatter {
-	lines = [];
 
-	// the director reads a new file and formats it, or handles non-special tokens
-	$_director(play, input, token) {
-		if (typeof input === "symbol") {
-			play(Symbol.for(NonSpecialToken), token);
-			return;
-		}
+lines = [];
 
-		const { lines } = this;
-		const path = input;
+// the director reads a new file and formats it, or handles non-special tokens
+$_director ( play, input, token ) {
 
-		const fileLines = readFileSync(path, "utf8").split(/\r?\n/);
-		for (const line of fileLines) {
-			play(Symbol.for(LineFormatter), line.trim());
-		}
+if ( typeof input === "symbol" ) {
 
-		return lines
-			.map((line) =>
-				line
-					.map((token) => token.formattedValue || token.value)
-					.join("")
-			)
-			.join("\n") + "\n";
-	}
+play ( Symbol .for ( NonSpecialToken ), token );
 
-	// the line formatter splits a line into tokens, calls the corresponding symbol direction for each token
-	$_LineFormatter(play, line) {
-		if (line == ""){
-			return
-		}
+return;
 
-		const { lines } = this;
+}
 
-		const tokens = jsTokens(line);
+const { lines } = this;
+const path = input;
+const fileLines = readFileSync ( path, "utf8" ) .split ( /\r?\n/ );
 
-		lines.push([]);
-		for (const token of tokens) {
-			if (token.done) break;
-			play(Symbol.for(token.type), token, lines);
-		}
+for ( const line of fileLines ) {
 
-		this.processNewLine();
-	}
+play ( Symbol .for ( LineFormatter ), line .trim () );
 
-	$_NonSpecialToken(play, token) {
-		const { lines } = this;
+}
 
-		const line = lines.at(-1);
-		const lastToken = line.at(-1);
+return lines
+ .map ( ( line ) =>
+line
+ .map ( ( token ) => token .formattedValue || token .value )
+ .join ( "" )
+)
+ .join ( "\n" ) + "\n";
 
-		if (lastToken) {
-			token.formattedValue = " " + token.value;
-		}
+}
 
-		line.push(token);
-	}
+// the line formatter splits a line into tokens, calls the corresponding symbol direction for each token
+$_LineFormatter ( play, line ) {
 
-	$_IdentifierName(play, token) {
-		const { lines } = this;
+if ( line == "" ) {
 
-		const line = lines.at(-1);
-		const lastToken = line.at(-1);
+return
 
-		if (lastToken) {
-			if (lastToken.value !== "." && lastToken.value !== "?.") {
-				token.formattedValue = " " + token.value;
-			}
-		}
+}
 
-		line.push(token);
-	}
+const { lines } = this;
+const tokens = jsTokens ( line );
 
-	$_Punctuator(play, token) {
-		const { lines } = this;
+lines .push ( [] );
+for ( const token of tokens ) {
 
-		const line = lines.at(-1);
-		const lastToken = line.at(-1);
+if ( token .done ) break;
+play ( Symbol .for ( token .type ), token, lines );
 
-		line.push(token);
+}
 
-		if (lastToken === undefined) {
-			return;
-		}
+this .processNewLine ();
 
-		switch (token.value) {
-			case ":":
-				const nextToLastToken = line.at(-2);
-				if (
-					lastToken.type !== "IdentifierName" ||
-					!["{", ","].includes(nextToLastToken?.value)
-				) {
-					token.formattedValue = " " + token.value;
-				}
+}
 
-				break;
+$_NonSpecialToken ( play, token ) {
 
-			case ("--", "++"):
-				if (lastToken?.type === "IdentifierName") break;
+const { lines } = this;
+const line = lines .at ( - 1 );
+const lastToken = line .at ( - 1 );
 
-			case ";":
-			case ",":
-				break;
+if ( lastToken ) {
 
-			case "[":
-				if (lastToken?.value !== "?.") {
-					token.formattedValue = " " + token.value;
-				}
+token .formattedValue = " " + token .value;
 
-				break;
+}
 
-			case ("]", ")", "}"):
-				if (lastToken?.value !== left[token.value]) {
-					token.formattedValue = " " + token.value;
-				}
+line .push ( token );
 
-				break;
+}
 
-			default:
-				token.formattedValue = " " + token.value;
-		}
-	}
+$_IdentifierName ( play, token ) {
 
-	$_MultiLineComment(play, token) {
-		const { lines } = this;
+const { lines } = this;
+const line = lines .at ( - 1 );
+const lastToken = line .at ( - 1 );
 
-		const line = lines.at(-1);
-		line.push(token);
-	}
-	
-	$_SingleLineComment(play, token) {
-		const { lines } = this;
+if ( lastToken ) {
 
-		const line = lines.at(-1);
-		line.push(token);
-	}
+if ( lastToken .value !== "." && lastToken .value !== "?." ) {
 
-	processNewLine() {
-		const { lines } = this;
+token .formattedValue = " " + token .value;
 
-		if (lines.length == 1) {
-			return;
-		}
+}
 
-		const lastLine = lines.at(-1);
+}
 
-		const nextToLastLine = lines.at(-2);
+line .push ( token );
 
-		if (
-			this.isBlockOpenning(nextToLastLine) ||
-			this.isBlockClosing(lastLine) ||
-			this.isBlockClosed(nextToLastLine) ||
-			this.haveAssignmentsEnded(nextToLastLine, lastLine) ||
-			this.haveDeclarationsEnded(nextToLastLine, lastLine) ||
-			this.haveFunctionCallsEnded(nextToLastLine, lastLine)
-		) {
-			nextToLastLine.push({
-				type: "LineTerminatorSequence",
-				value: "\n",
-			});
-		}
-	}
+}
 
-	$_LineTerminatorSequence(play, token) {
-		// skip
-	}
+$_Punctuator ( play, token ) {
 
-	$_WhiteSpace(play, token, output) {
-		// skip
-	}
+const { lines } = this;
+const line = lines .at ( - 1 );
+const p = Scenarist ( new Punctuator () )
 
-	isBlockClosed(nextToLastLine) {
-		return nextToLastLine.length > 0 && nextToLastLine[0].value == "}";
-	}
+p ( token, line )
 
-	isLastLineNewLine(nextToLastLine) {
-		return (
-			nextToLastLine.length > 0 &&
-			nextToLastLine[0].type == "LineTerminatorSequence"
-		);
-	}
+// play(Scenarist(new Punctuator()), token, line)
 
-	isBlockOpenning(nextToLastLine) {
-		const len = nextToLastLine.length;
-		return len > 0 && nextToLastLine.at(-1).value === "{";
-	}
+}
 
-	isBlockClosing(lastLine) {
-		return lastLine.length > 0 && lastLine[0].value == "}";
-	}
+$_MultiLineComment ( play, token ) {
 
-	haveDeclarationsEnded(nextToLastLine, lastLine) {
-		return (
-			nextToLastLine.length > 0 &&
-			lastLine.length > 0 &&
-			declarationKeyWords.includes(nextToLastLine[0].value) &&
-			!declarationKeyWords.includes(lastLine[0].value)
-		);
-	}
+const { lines } = this;
+const line = lines .at ( - 1 );
 
-	haveAssignmentsEnded(nextToLastLine, lastLine) {
-		return (
-			nextToLastLine.length > 1 &&
-			assignmentOperators.includes(nextToLastLine[1].value) &&
-			(lastLine.length < 2 ||
-				!assignmentOperators.includes(lastLine[1].value))
-		);
-	}
+line .push ( token );
 
-	haveFunctionCallsEnded(nextToLastLine, lastLine) {
-		return (
-			nextToLastLine.length > 1 &&
-			nextToLastLine[1].value == "(" &&
-			(lastLine.length < 2 || lastLine[1].value != "(")
-		);
-	}
+}
+
+$_SingleLineComment ( play, token ) {
+
+const { lines } = this;
+const line = lines .at ( - 1 );
+
+line .push ( token );
+
+}
+
+processNewLine () {
+
+const { lines } = this;
+
+if ( lines .length == 1 ) {
+
+return;
+
+}
+
+const lastLine = lines .at ( - 1 );
+const nextToLastLine = lines .at ( - 2 );
+
+if (
+
+this .isBlockOpenning ( nextToLastLine ) ||
+this .isBlockClosing ( lastLine ) ||
+this .isBlockClosed ( nextToLastLine ) ||
+this .haveAssignmentsEnded ( nextToLastLine, lastLine ) ||
+this .haveDeclarationsEnded ( nextToLastLine, lastLine ) ||
+this .haveFunctionCallsEnded ( nextToLastLine, lastLine )
+) {
+
+nextToLastLine .push ( {
+
+type : "LineTerminatorSequence",
+value : "\n",
+
+} );
+
+}
+
+}
+
+$_LineTerminatorSequence ( play, token ) {
+
+// skip
+
+}
+
+$_WhiteSpace ( play, token ) {
+
+// skip
+
+}
+
+isBlockClosed ( nextToLastLine ) {
+
+return nextToLastLine .length > 0 && nextToLastLine [ 0 ] .value == "}";
+
+}
+
+isLastLineNewLine ( nextToLastLine ) {
+
+return (
+
+nextToLastLine .length > 0 &&
+nextToLastLine [ 0 ] .type == "LineTerminatorSequence"
+);
+
+}
+
+isBlockOpenning ( nextToLastLine ) {
+
+const len = nextToLastLine .length;
+
+return len > 0 && nextToLastLine .at ( - 1 ) .value === "{";
+
+}
+
+isBlockClosing ( lastLine ) {
+
+return lastLine .length > 0 && lastLine [ 0 ] .value == "}";
+
+}
+
+haveDeclarationsEnded ( nextToLastLine, lastLine ) {
+
+return (
+
+nextToLastLine .length > 0 &&
+lastLine .length > 0 &&
+declarationKeyWords .includes ( nextToLastLine [ 0 ] .value ) &&
+ ! declarationKeyWords .includes ( lastLine [ 0 ] .value )
+);
+
+}
+
+haveAssignmentsEnded ( nextToLastLine, lastLine ) {
+
+return (
+
+nextToLastLine .length > 1 &&
+assignmentOperators .includes ( nextToLastLine [ 1 ] .value ) &&
+ ( lastLine .length < 2 ||
+ ! assignmentOperators .includes ( lastLine [ 1 ] .value ) )
+);
+
+}
+
+haveFunctionCallsEnded ( nextToLastLine, lastLine ) {
+
+return (
+
+nextToLastLine .length > 1 &&
+nextToLastLine [ 1 ] .value == "(" &&
+ ( lastLine .length < 2 || lastLine [ 1 ] .value != "(" )
+);
+
+}
+
+}
+
+class Punctuator {
+
+$_director ( play, input1, input2, input3 ) {
+
+if ( typeof input1 === "symbol" ) {
+
+input2 .formattedValue = " " + input2 .value;
+input3 .push ( input2 )
+return
+
+}
+
+play ( Symbol .for ( PunctuatorType [ input1 .value ] ), input1, input2 )
+
+}
+
+$_Colon ( play, token, line ) {
+
+const lastToken = line .at ( - 1 );
+
+line .push ( token );
+if ( lastToken === undefined ) {
+
+return;
+
+}
+
+const nextToLastToken = line .at ( - 2 );
+
+if (
+
+lastToken .type !== "IdentifierName" ||
+ ! [ "{", "," ] .includes ( nextToLastToken ?.value )
+) {
+
+token .formattedValue = " " + token .value;
+
+}
+
+}
+
+$_IncrementDecrement ( play, token, line ) {
+
+const lastToken = line .at ( - 1 );
+
+line .push ( token );
+if ( lastToken === undefined ) {
+
+return;
+
+}
+
+if ( lastToken ?.type !== "IdentifierName" ) {
+
+token .formattedValue = " " + token .value;
+
+}
+
+}
+
+$_SemiColon ( play, token, line ) {
+
+const lastToken = line .at ( - 1 );
+
+line .push ( token );
+
+}
+
+$_Comma ( play, token, line ) {
+
+const lastToken = line .at ( - 1 );
+
+line .push ( token );
+
+}
+
+$_OpeningSquareBracket ( play, token, line ) {
+
+const lastToken = line .at ( - 1 );
+
+line .push ( token );
+if ( lastToken === undefined ) {
+
+return;
+
+}
+
+if ( lastToken ?.value !== "?." ) {
+
+token .formattedValue = " " + token .value;
+
+}
+
+}
+
+$_Closing ( play, token, line ) {
+
+const lastToken = line .at ( - 1 );
+
+line .push ( token );
+if ( lastToken === undefined ) {
+
+return;
+
+}
+
+if ( lastToken ?.value !== left [ token .value ] ) {
+
+token .formattedValue = " " + token .value;
+
+}
+
+}
+
 }
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import Formatter from "./formatter.js";
 import Scenarist from "scenarist.dev";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { writeFileSync } from "fs";
+const options = yargs ( hideBin ( process .argv ) )
 
-const play = Scenarist(new Formatter());
-const path = "./formatter.js";
-const output = play(path);
-console.log(output);
+ .usage ( "Usage: -p <path>" )
+ .option ( "p", { alias : "path", describe : "File to be formatted", type : "string", demandOption : true } )
+ .boolean ( "write" ) .alias ( 'write', [ 'w' ] ) .describe ( 'write', "Whether to overwrite file with formatted version" ) .argv;
+const play = Scenarist ( new Formatter () );
+const output = play ( options .path );
+
+if ( options .write ) {
+
+writeFileSync ( options .path, output )
+
+} else {
+
+console .log ( output );
+
+}
